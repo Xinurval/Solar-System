11df58c63ca15f21ccaec72614aa5f0828048eea
Fallows <furytheepic@gmail.com>
2024-07-31 10:48:59 +0100

    Feat: Created UI for buttons and added generate planet button

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ let G = 50;
 let numPlanets = 5; 
 let destabilise = 0.2;
 
+let newPlanetButton;
+let deletePlanetButton;
+
+
 // setup code here
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -13,37 +17,34 @@ function setup() {
 
 	// create planets
 	for (let i = 0; i < numPlanets; i++){
-		// planet position
-		// find random radius to place planet
-		let r = random(sun.r, min(windowWidth/ 2, windowHeight/2));
-		// find random angle to place planet 
-		let theta = random(TWO_PI);
-		// calculate position of planet
-		let planetPos = createVector(r * cos(theta), r * sin(theta));
-
-		// planet color
-		let planetColor = color(random(0, 255), random(0, 255), random(0, 255));
-
-		// planet velocity
-		let planetVel = planetPos.copy();
-		// set velocity to act at a right angle to planet position from sun 
-		planetVel.rotate(HALF_PI);
-		// calculate velocity using orbital speed physics equation
-		planetVel.setMag(sqrt(G * sun.mass/ planetPos.mag()));
-		// reverse direction of planets 20% of time
-		if (random(1) < 0.2){
-			planetVel.mult(-1);
-		}
-		// make orbit ellipitcal instead of circular by changing velocity 
-		planetVel.mult(random(1 - destabilise, 1 + destabilise));
-		planets.push( new Body(random(5, 30), planetPos, planetVel, planetColor) );
+		planets.push( generatePlanet() )
 	}
 
-	// setup text
+	// setup title text
 	font = loadFont('/Montserrat-ExtraBold.ttf');
 	textFont(font);
 	textSize(windowWidth / 50);
 	textAlign(CENTER, CENTER);
+
+	// button to generate new planet
+	newPlanetButton = createButton("NEW");
+  newPlanetButton.mouseClicked(() => planets.push(generatePlanet()));
+  newPlanetButton.position(windowWidth - 132, 15);
+	newPlanetButton.size(107, 62);
+  newPlanetButton.style("font-family", font); 
+	newPlanetButton.style("font-size", "32px");
+	newPlanetButton.style("background-color", color(255, 255, 255));
+	newPlanetButton.style("border-radius", "5px");
+
+	// button to delete planet
+	deletePlanetButton = createButton("DELETE");
+	// button.mouseClicked(generatePlanet);
+	deletePlanetButton.position(windowWidth - 132, 80);
+	deletePlanetButton.size(107, 62);
+	deletePlanetButton.style("font-family", font); 
+	deletePlanetButton.style("font-size", "28px");
+	deletePlanetButton.style("background-color", color(255, 255, 255));
+	deletePlanetButton.style("border-radius", "5px");
 }
 
 function draw() {	
@@ -54,6 +55,9 @@ function draw() {
 	
 	// display text
 	text("Solar System Simulation", 20, - (windowHeight / 2) + (windowHeight / 14));
+
+	// draw ui for modifying planets
+	rect(width / 2 - 220, -height / 2 + 10, 200, 300, 5)
 
 	// draw planets
 	for (let i = 0; i < planets.length; i++){
@@ -70,6 +74,33 @@ function windowResized() {
 	textSize(windowWidth / 50);
 }
 
+// function to generate planets
+function generatePlanet(){
+	// planet position
+	// find random radius to place planet
+	let r = random(sun.r, min(windowWidth/ 2, windowHeight/2));
+	// find random angle to place planet 
+	let theta = random(TWO_PI);
+	// calculate position of planet
+	let planetPos = createVector(r * cos(theta), r * sin(theta));
+
+	// planet color
+	let planetColor = color(random(0, 255), random(0, 255), random(0, 255));
+
+	// planet velocity
+	let planetVel = planetPos.copy();
+	// set velocity to act at a right angle to planet position from sun 
+	planetVel.rotate(HALF_PI);
+	// calculate velocity using orbital speed physics equation
+	planetVel.setMag(sqrt(G * sun.mass/ planetPos.mag()));
+	// reverse direction of planets 20% of time
+	if (random(1) < 0.2){
+		planetVel.mult(-1);
+	}
+	// make orbit ellipitcal instead of circular by changing velocity 
+	planetVel.mult(random(1 - destabilise, 1 + destabilise));
+	return new Body(random(5, 30), planetPos, planetVel, planetColor)
+}
 
 // class for celestial bodies		
 function Body(_mass, _pos, _vel, _color){
